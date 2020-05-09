fb03ace47a9cc16034c29a55101065638ef4e1b3
Aarav-coder <62931789+Aarav-coder@users.noreply.github.com>
2020-05-09 13:38:15 +0530

    visibility and ascii

--- Slingshot.js
+++ Slingshot.js
@@ -17,7 +17,9 @@ class SlingShot{
     fly(){
         this.sling.bodyA = null;
     }
-
+attach(body){
+    this.sling.bodyA = body;
+}
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
@@ -39,6 +41,7 @@ class SlingShot{
                 image(this.sling3,pointA.x+25,pointA.y-10,15,30);    
             }
             pop();
+            
         }
     }
     
